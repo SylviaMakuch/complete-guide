1f247e68bc8cb89fbe28868eef0c71edb1799ba0
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-10-31 23:02:50 -0700

    New Form

---
+++ src/components/NewExpense.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm.js";
+
+const NewExpense = () =>{
+    return <div>
+        <ExpenseForm />
+    </div>
+
+};
+export default NewExpense;
