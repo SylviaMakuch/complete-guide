195182ca133c7c0015a70edab718324bf6ca555d
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-11-13 19:45:50 -0800

    small bug,

--- src/components/ExpenseChart.js
+++ src/components/ExpenseChart.js
@@ -19,7 +19,9 @@ const ExpensesChart = (props) => {
 
   for (const expense of props.expenses) {
     const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
-    chartDataPoints[expenseMonth].value += expense.amount;
+    chartDataPoints[expenseMonth].value += (expense.amount);
+    console.log(chartDataPoints[expenseMonth].value);
+
   }
 
   return <Chart dataPoints={chartDataPoints} />;
--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,6 +1,6 @@
 import './ExpenseDate.css';
 
-export default function ExpenseDate(props) {
+function ExpenseDate(props) {
   console.log(props);
   const month = props.date?.toLocaleString('en-US', { month: 'long' });
   const day = props.date?.toLocaleString('en-US', { day: '2-digit' });
@@ -14,3 +14,4 @@ export default function ExpenseDate(props) {
     </div>
   );
 }
+export default ExpenseDate;
